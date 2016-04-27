9e49059a20d66763b76c17d969fe1c5b5c4de42a
Jarmo Habakuk <jarmhab@users.noreply.github.com>
2016-04-27 09:59:11 +0300

    added first lines to app.js

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-// Jarmo tehtud kood
+(function(){
+  "use strict";
 
-var kood="kood";
+  var Discgolf = function(){
+    //Singelton
+    if(Discgolf.instance){
+      return Discgolf.instance;
+    }
+    Discgolf.instance = this;
 
+    //Rajad tulevad failist tracks.js
+    this.tracks = tracks;
 
-// Koidu tehtud kood
+    this.init();
+  };
+
+  window.Discgolf = Discgolf;
+
+  Discgolf.prototype = {
+    init: function(){
+
+      console.log(this.track_list);
+
+      console.log(this.track_list[0]);
+      console.log(this.track_list[0].name);
+      console.log(this.track_list[0].baskets.length);
+      for(var i = 0; i < this.track_list[0].baskets.length; i++){
+         console.log(this.track_list[0].baskets[i].nr + " " + this.track_list[0].baskets[i].par);
+      }
+    }
+  };
+
+})();
