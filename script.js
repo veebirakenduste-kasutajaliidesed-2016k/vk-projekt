0af4e54b3647bdd0d27af4150e136e8c0882d131
root <root@robing.tech>
2016-05-02 09:06:35 -0400

    Some progress, but street view is causing issues

--- script.js
+++ script.js
@@ -1,9 +1,86 @@
 var map;
+//TODO: set image size to mapSize, generate marker location and image locations randomly
+//Get geocoder status
+//https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&location_type=ROOFTOP&result_type=street_address&key=AIzaSyCl5QWc_dxpap3_ITxjyq0s3MyB_V-xplI
 function initMap() {
-  var mapSize = (screen.width/3).toString() + "," + (screen.witdh/1.42).toString();
+  var latitude;
+  var longitude;
+  var latlng;
+  var mapSize = Math.round(screen.width/3).toString() + "x" + Math.round(screen.width/1.42).toString();
+  var firstImage = document.getElementById('firstImage');
+  var secondImage = document.getElementById('secondImage');
+  var thirdImage = document.getElementById('thirdImage');
+  function randomInt(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  function getLatLong(){
+    latitude =  randomInt(40, 70);
+    longitude = randomInt(-10, 30);
+    latlng = latitude.toString() + "," + longitude.toString();
+  }
+  //var latlng = lat.toStringUI + "," + long.toString();
+  //var latlng = new google.maps.LatLng(latitude,longitude);
+  //console.log("lat: "+ latitude.toString());
+  //console.log("long: "+ longitude.toString());
+  console.log(mapSize);
+  checkAddress(latlng);
+  //var geocoder = new google.maps.Geocoder;
+  function streetView(maxDistance){
+    /*Kind of works, but google causes issues
+    var streetViewService = new google.maps.StreetViewService();
+    streetViewService.getPanoramaByLocation(google_latlng, maxDistance, function (streetViewPanoramaData, status) {
+      if (status === google.maps.StreetViewStatus.OK) {
+          console.log(status);
+          console.log("http://maps.googleapis.com/maps/api/streetview?size="+mapSize.toString()+"&location="+latlng.toString()+"&fov=120&heading=235&pitch=10&sensor=false");
+          firstImage.src = "http://maps.googleapis.com/maps/api/streetview?size="+mapSize.toString()+"&location="+latlng.toString()+"&fov=120&heading=235&pitch=10&sensor=false";
+      } 
+      else {
+        console.log("Streetview not available, increasing max distance");
+        streetView(maxDistance + 1000);
+      }
+    });
+*/
+  }
+  function checkAddress() {
+    getLatLong();
+    google_latlng = new google.maps.LatLng(latitude,longitude);
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'latLng': google_latlng}, function(results, status) {
+      //console.log(results);
+      console.log(status);
+      if(status != google.maps.GeocoderStatus.OK) {
+        //checkAddress(latlng);
+        setTimeout(function() {checkAddress();}, 200);
+      }
+      else if(status == google.maps.GeocoderStatus.OK) {
+        console.log("lat: "+ latitude.toString());
+        console.log("long: "+ longitude.toString());
+        //console.log("http://maps.googleapis.com/maps/api/streetview?size="+mapSize.toString()+"&location="+latlng.toString()+"&fov=120&heading=235&pitch=10&sensor=false");
+        var marker = new google.maps.Marker({
+          position: {lat: latitude, lng: longitude},
+          map: map
+        });
+        streetView(1000);
+
+      }
+    });
+  }
+  /*geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+          var locationStatus = true;         
+      }else {
+          var latitude = randomInt(-90, 90);
+          var longitude = randomInt(-180, 180);
+          var latlng = new google.maps.LatLng(latitude,longitude);
+          console.log("Geocode failed");
+          var geocoder = new google.maps.Geocoder;
+          geocoder.geocode(latlng);
+      }
+  });*/
   map = new google.maps.Map(document.getElementById('map'), {
     //center: {lat: -34.397, lng: 150.644},
-    center: {lat: 40, lng:0},
+    //center: {lat: latitude, lng:longitude},
+    center: {lat: 0, lng:0},
     zoom: 2,
     disableDefaultUI: true,
     mapTypeControl: false,
@@ -16,9 +93,8 @@ function initMap() {
     panControl: false
 
   });
-  var marker = new google.maps.Marker({
-  position: {lat: 10, lng: 15},
-  map: map,
-  title: 'Hello World!'
-  });
+
+  //Check that the first number in your latitude coordinate is between -90 and 90
+  //and the first number in your longitude coordinate is between -180 and 180.
+  
 }
