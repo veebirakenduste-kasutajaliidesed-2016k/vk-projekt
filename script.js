8c7a93f847f46d507577e44348c0cca0e18ebe2f
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2016-05-02 20:45:53 +0300

    single page application

--- script.js
+++ script.js
@@ -6,16 +6,45 @@
       return TeadmisteTest.instance;
     }
     TeadmisteTest.instance = this;
+    this.routes = TeadmisteTest.routes;
+    this.currentRoute = null;
     //siia ülejäänud muutujad
     this.init();
   };
 
   window.TeadmisteTest = TeadmisteTest;
 
+  TeadmisteTest.routes = {
+    'home-view': {
+      'render': function(){}
+    },
+    'insert-view': {
+      'render': function(){}
+    },
+    'test-view': {
+      'render': function(){}
+    }
+  };
+
   TeadmisteTest.prototype = {
 
     init: function(){
 
+      window.addEventListener('hashchange', this.routeChange.bind(this));
+      if(!window.location.hash){
+        window.location.hash = 'home-view';
+      }else{
+        this.routeChange();
+      }
+
+    },
+
+    routeChange: function(){
+      console.log('this is', location.hash.substr(1));
+      this.currentRoute = location.hash.slice(1);
+      if(this.routes[this.currentRoute]){
+        this.routes[this.currentRoute].render();
+      }
     },
     //siia ülejäänud meetodid
   };
