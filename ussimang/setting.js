939aff846510157a8bf8cc29b1ef20b9f08aef4e
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2016-05-02 23:22:30 +0300

    Algne versioon

---
+++ ussimang/setting.js
@@ -0,0 +1,9 @@
+var mycanvas = document.getElementById('mycanvas');
+var ctx = mycanvas.getContext('2d');
+var snakeSize = 20;
+var w = 700;
+var h = 700;
+var score = 0;
+var snake;
+var snakeSize = 20;
+var food;
