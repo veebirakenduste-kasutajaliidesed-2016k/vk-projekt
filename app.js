5e10b752ba7fe741beb32577b809cdf0604a0f37
oleloigu <oleloigu@users.noreply.github.com>
2016-05-06 21:27:37 +0300

    geoloc added

--- app.js
+++ app.js
@@ -7,40 +7,56 @@
         }
         
     frontMap.instance = this;
-        console.log('inside map');
     
-
-    this.init()
+    this.geoLocation();
     };
     
     window.frontMap = frontMap;
     
     frontMap.prototype = {
       
-        init: function(){
+        init: function(position) {
             if(navigator.onLine) {
                 this.map;
-                this.center = new google.maps.LatLng(59.436984, 24.750286);
+                this.center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                 console.log('working'); 
-            this.initialize();
-            } else{
-                this.window.alert('You are offline. Please connect to the internet');
-            }
-
-    },
+                this.initialize();
+                
+            } else {
+                
+                window.alert('You are offline. Please connect to the internet');
+            }},
         
     initialize: function() {
         this.map = new google.maps.Map(document.getElementById('map'), {
         center: this.center,
-        zoom: 13
-
-    });   
-    },
-
-    };
-            
+        zoom: 13,
     
-   window.onload = function(){
+            
+
+    });
+    },
+    
+    geoLocation: function() {
+       var geolocation = null;
+
+        if (window.navigator && window.navigator.geolocation) {
+            this.geolocation = window.navigator.geolocation;
+        }
+        if (this.geolocation) {
+            this.geolocation.getCurrentPosition(function(position) {
+            frontMap.instance.init(position);
+            console.log(position);
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+            
+                
+        });
+    }},
+      
+};
+            
+   window.onload = function() {
      var app = new frontMap();
    };
 
