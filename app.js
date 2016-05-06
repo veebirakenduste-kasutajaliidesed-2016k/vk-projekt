32a5bef001ce8cc84cda9bb5f488cc7961e7c2ca
oleloigu <oleloigu@users.noreply.github.com>
2016-05-06 17:05:04 +0300

    fixed offline issue

--- app.js
+++ app.js
@@ -10,7 +10,6 @@
         console.log('inside map');
     
 
-    
     this.init()
     };
     
@@ -19,29 +18,25 @@
     frontMap.prototype = {
       
         init: function(){
-            this.map;
-            this.center = new google.maps.LatLng(59.436984, 24.750286);
-            console.log('working');
-                 
-        this.initialize();
-            
+            if(navigator.onLine) {
+                this.map;
+                this.center = new google.maps.LatLng(59.436984, 24.750286);
+                console.log('working'); 
+            this.initialize();
+            } else{
+                this.window.alert('You are offline. Please connect to the internet');
+            }
+
     },
         
     initialize: function() {
         this.map = new google.maps.Map(document.getElementById('map'), {
         center: this.center,
         zoom: 13
+
     });   
-        
     },
-            
-    callBack: function(status){
-        if(this.status == google.maps.places.PlacesServiceStatus.OK){
-            console.log('status ok');
-        }else{
-            console.log('status not ok');
-        }
-    },
+
     };
             
     
