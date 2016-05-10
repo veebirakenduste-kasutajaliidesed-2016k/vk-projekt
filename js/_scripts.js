ae94e8ad1f7e6a8ac4e0cce2ae50fac17d37e99c
Priit Puru <purupriit@gmail.com>
2016-05-10 23:11:28 +0300

    Even more code

--- js/_scripts.js
+++ js/_scripts.js
@@ -12,6 +12,8 @@
     // KÕIK muuutujad, mida muudetakse ja on rakendusega seotud defineeritakse siin
     this.cookies = 0;
     this.cps = 0;
+    this.upgrade = [0,0,0,0,0];
+    this.upgradeCPS = [1, 10, 100, 250, 1000];
     console.log(this);
 
     this.init();
@@ -23,9 +25,13 @@
 
     init: function(){
     console.log('Rakendus läks tööle');
-    this.cookies = parseInt(localStorage.getItem("cookies"));
-    this.cps = parseInt(localStorage.getItem("cps"));
-    console.log("###GAME LOADED###");
+    if(parseInt(localStorage.getItem("cookies"))){
+      this.load();
+      console.log("###GAME LOADED###");
+    }else{
+      console.log("###NO SAVE FOUND###");
+    }
+    this.updateCookieAmount();
     this.bindEvents();
     this.main();
     console.log(this.cookies);
@@ -33,34 +39,67 @@
     main: function(){
       var game = this;
       setInterval(function(){
-        game.cookies += game.cps;
+        game.addCookie(game.cps);
+        game.updateCookieAmount();
         console.log(game.cookies); 
-        localStorage.setItem("cookies", game.cookies);
-        localStorage.setItem("cps", game.cps);
+        console.log("CPS: "+game.cps);
+        game.save();
       }, 1000);
     },
     bindEvents: function(){
       var game = this;
       $('.cookie').click(function(){
         game.addCookie(1);
+        game.updateCookieAmount();
+      });
+      $('.upgrade').click(function(){
+        this.Index = $(".upgrade").index(this);
+        console.log("upgrade" + this.Index);
+        game.upgradeSkills(this.Index);
       });
       $('.autoclicker').click(function(){
-        game.cps += 1;
-      });
-      $('.save').click(function(){
-        console.log("Game Saved");
+        
       });
       $('.delete').click(function(){
-        localStorage.removeItem("cookies");
-        localStorage.removeItem("cps");
-        game.cookies = 0;
-        game.cps = 0;
-        console.log("Save Deleted");
+        game.delete();
       });
     },
     addCookie: function(amount){
       this.cookies += amount;
-      console.log(this.cookies);
+    },
+    updateCookieAmount: function(){
+      $('.cookieamount').html(this.cookies);
+    },
+    save: function(){
+      localStorage.setItem("cookies", this.cookies);
+      localStorage.setItem("cps", this.cps);
+      localStorage.setItem("upgrade1", this.upgrade1);
+    },
+    delete: function(){
+      localStorage.removeItem("cookies");
+      localStorage.removeItem("cps");
+      localStorage.removeItem("upgrade1");
+      this.cookies = 0;
+      this.cps = 0;
+      this.upgrade1 = 0;
+      console.log("Save Deleted");
+    },
+    load: function(){
+      this.cookies = parseInt(localStorage.getItem("cookies"));
+      this.cps = parseInt(localStorage.getItem("cps"));
+      this.upgrade1 = parseInt(localStorage.getItem("upgrade1"));
+    },
+    upgradeSkills: function(index){
+      var cost = Math.floor(Math.pow(10,index) * Math.pow(1.1,this.upgrade[index]));
+      if(this.cookies>cost && this.cookies-cost>0){
+        this.cookies -= cost;
+        console.log(this.upgrade[index]);
+        this.upgrade[index] += 1;
+        this.cps += this.upgradeCPS[index];
+        this.updateCookieAmount();
+      }else{
+        console.log("Need mo money");
+      }        
     },
   }
     window.onload = function(){
