43375b9fecff7a6d03b55767bd7cfa6a99ac2950
Raiko <raikolepik@users.noreply.github.com>
2016-05-10 11:21:10 +0300

    andmebaasi salvestus

--- save.php
+++ save.php
@@ -7,7 +7,7 @@ $mysqli = new mysqli($servername, $server_username, $server_password, $database)
 
 function addScore($name, $score, $mysqli) {
 
-  $stmt = $mysqli->prepare("INSERT INTO brickmang (name, score, created) VALUES (?, ?, NOW())");
+  $stmt = $mysqli->prepare("INSERT INTO brickmang (name, score, date) VALUES (?, ?, NOW())");
   $stmt->bind_param("si", $name, $score);
 
   if ($stmt->execute()) {
@@ -17,7 +17,7 @@ function addScore($name, $score, $mysqli) {
 }
 
 if(isset($_GET['name']) && isset($_GET['score'])) {
-  addScore($_GET['name'], $_GET['score']);
+  addScore($_GET['name'], $_GET['score'], $mysqli);
 }
 
 ?>
