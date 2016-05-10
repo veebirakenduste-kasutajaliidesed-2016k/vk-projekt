05d2f5dcd8cee27133e43ca3b59126477e4da904
Rauno Kosula <Raunts26@users.noreply.github.com>
2016-05-10 18:33:57 +0000

    #9 php top 10 kuvamiseks

---
+++ get.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("config.php");
+$database = "vhost45490s3";
+
+$mysqli = new mysqli($servername, $server_username, $server_password, $database);
+
+function topTen($mysqli) {
+
+  $stmt = $mysqli->prepare("SELECT name, score FROM brickmang ORDER BY score DESC LIMIT 10");
+  $stmt->bind_result($name, $score);
+
+  $stmt->execute();
+
+  $array = array();
+
+  while($stmt->fetch()) {
+    $player = new StdClass();
+    $player->name = $name;
+    $player->score = $score;
+    array_push($array, $player);
+  }
+
+  return($array);
+  $stmt->close();
+}
+
+if(isset($_GET['topten'])) {
+  topTen($mysqli);
+}
+
+?>
