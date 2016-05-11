9fd76a711a93b718a9f7a8cb0fb2104fab12b3f2
Kevin Münter <Kevin.mynter@gmail.com>
2016-05-11 09:17:00 +0300

    Upgrade

--- createData.php
+++ createData.php
@@ -2,16 +2,38 @@
 	require_once("simple_html_dom.php");
 # http://php.net/manual/en/function.file-get-contents.php
 
-  $words = ['paks'];
+  $words = ['paks', 'rõõmus'];
 
-  results($words[0]);
+	$newArray = array();
+
+	foreach (  $words as $key => $word) {
+		$syns = results($word);
+
+		//echo count(	$syns);
+
+		if(count(	$syns) > 3){
+
+
+
+			$o = new stdClass();
+			$o->word = $word;
+			$o->syns = $syns;
+
+			array_push($newArray, $o);
+		}
+
+
+	}
+	echo count($newArray);
+	echo json_encode($newArray);
+
 
 function results ($search){
 	//$title, $description, $image, $url
 	$contents = file_get_contents('http://www.eki.ee/dict/sys/index.cgi?Q='.$search.'&F=M&C06=et');
 	//var_dump($contents);
 
-	$html = str_get_html($contents);
+	$html = str_get_html( mb_convert_encoding($contents, 'HTML-ENTITIES', 'UTF-8'));
 
 	//http://simplehtmldom.sourceforge.net/manual.htm
 
@@ -24,10 +46,11 @@ function results ($search){
       array_push($syns, $syn);
 
       //obje
-			echo $syn;
-			echo " ";
+			//echo $syn;
+			//echo " ";
 	}
-  //json_decode[]
+
+	return $syns;
   //var_dump($syns);
 
 
@@ -36,6 +59,6 @@ function results ($search){
 }
 
 
-	$file_name = "cache.txt";
+	//$file_name = "cache.txt";
 
-	$requestMethod = "GET";
+	//$requestMethod = "GET";
