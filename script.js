5a035e13f43078f5adc317733b5ff4cc145e6bc0
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2016-05-15 19:15:41 +0300

    saves words into file and localstorage

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     TeadmisteTest.instance = this;
     this.routes = TeadmisteTest.routes;
     this.currentRoute = null;
-    //siia ülejäänud muutujad
+    this.words = [];
     this.init();
   };
 
@@ -29,14 +29,13 @@
   TeadmisteTest.prototype = {
 
     init: function(){
-
       window.addEventListener('hashchange', this.routeChange.bind(this));
       if(!window.location.hash){
         window.location.hash = 'home-view';
       }else{
         this.routeChange();
       }
-
+      this.listenToMouse();
     },
 
     routeChange: function(){
@@ -46,7 +45,34 @@
         this.routes[this.currentRoute].render();
       }
     },
-    //siia ülejäänud meetodid
+
+    listenToMouse: function(){
+      document.querySelector('.add-new-word').addEventListener('click', this.addNewWord.bind(this));
+    },
+
+    addNewWord: function(event){
+      var estonian_word = document.querySelector('.estonian-word').value;
+      var english_word = document.querySelector('.english-word').value;
+      var new_word = new Word(estonian_word, english_word);
+      this.words.push(new_word);
+      localStorage.setItem('words', JSON.stringify(this.words));
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          console.log('salvestan selverisse');
+        }
+      };
+      xhttp.open("GET", "saveWords.php?estonian_word=" + estonian_word +  "&english_word=" + english_word, true);
+      xhttp.send();
+    },
+
+
+
+  };
+
+  var Word = function(new_estonian_word, new_english_word){
+    this.estonian_word = new_estonian_word;
+    this.english_word = new_english_word;
   };
 
   window.onload = function(){
