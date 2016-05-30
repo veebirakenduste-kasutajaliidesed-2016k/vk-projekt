2802b89c440027237480726a4bc426ed559dc3fb
Kristian Talviste <kristal@greeny.cs.tlu.ee>
2016-05-30 17:46:09 +0300

    working authentication and primitive request

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 SC.initialize({
-  client_id: 'af8624e04e20f5c2f2112fa49cada87a'
+  client_id: 'af8624e04e20f5c2f2112fa49cada87a',
+  redirect_uri: 'http://sc-callback.azurewebsites.net/callback.html'
 });
 
-$(document).ready(function() {
-  SC.get('/tracks', { genres: 'muse' }, function(tracks) {
-    $(tracks).each(function(index, track) {
-      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-
-      var track_url = 'http://soundcloud.com/forss/flickermood';
-      SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-        console.log('oEmbed response: ', oEmbed);
-      });
-
-      SC.stream('/tracks/293', function(sound) {
-        $('#start').click(function(e) {
-          e.preventDefault();
-          sound.start();
-        });
-        $('#stop').click(function(e) {
-          e.preventDefault();
-          sound.stop();
-        });
-      });
-      });
-    });
+SC.connect(function(){
+  SC.get("/me", function(me){
+    $("#username").text(me.username);
+  });
 });
+
+(function(){
+    "use strict";
+
+    var App = function(){
+
+        if(App.instance){
+        return App.instance;
+    }
+
+    App.instance = this;
+
+    this.run();
+
+    };
+
+    window.App = App;
+
+/*var track_url = 'https://soundcloud.com/wearegalantis/no-money-ultra-edit-126-bpm-1';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});*/
+
+
+
+  App.prototype = {
+
+    search: function(event){
+        var search = document.querySelector('#search').value.toLowerCase();
+        console.log(search);
+
+        SC.get('/tracks', { genres: search }, function(tracks) {
+          $(tracks).each(function(index, track) {
+            $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+          });
+        });
+    }
+
+  };
+
+  window.onload = function(){
+      var t = new App();
+      };
+})();
