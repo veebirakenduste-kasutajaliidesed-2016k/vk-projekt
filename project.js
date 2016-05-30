1daac638121ae4069cb0958b7eaef54d74022d3a
Ruslan Jegorov <rusljeg@greeny.cs.tlu.ee>
2016-05-30 13:54:37 +0300

    html

---
+++ project.js
@@ -0,0 +1,24 @@
+var users = [
+	{	username: "user1",
+		password: "user1"
+	},
+	{
+		username: "user2",
+		password: "user2"
+	}
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < users.length; i++) {
+		if(username == users[i].username && password == users[i].password) {
+			console.log(username + " is logged in!!!")
+			location=("project.html");
+			return
+		}
+	}
+	console.log("incorrect username or password")
+}
