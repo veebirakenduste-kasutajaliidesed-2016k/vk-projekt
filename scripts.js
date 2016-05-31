e7a824ebd2e8a63a9dab1456701a6a49b140faa1
Your Name <you@example.com>
2016-05-31 09:34:28 +0300

    lisatud senine töötav: skoori lugemine

--- scripts.js
+++ scripts.js
@@ -15,9 +15,11 @@
 
         this.sides_list = sides_list;
         this.new = true;
+
+        //skoor
         this.guessed_sides = 0;
 
-        //this.counter = 10;
+        //this.player = {name: null, score: 0};
 
 
         this.init();
@@ -28,6 +30,9 @@
     Sides.prototype = {
 
         init: function(){
+
+          //this.loadPlayerData();
+
           var counter = 10;
           var id;
 
@@ -35,6 +40,7 @@
           console.log('Sides started');
 
           Sides.instance.writeRandomSide();
+          Sides.instance.writeScore();
           window.addEventListener("devicemotion", this.handleMotionRight.bind(this));
           window.addEventListener("devicemotion", this.handleMotionLeft.bind(this));
           window.addEventListener("devicemotion", this.handleMotionFront.bind(this));
@@ -42,121 +48,133 @@
           window.addEventListener("devicemotion", this.handleMotionUp.bind(this));
           window.addEventListener("devicemotion", this.handleMotionDown.bind(this));
 
+
           id = setInterval(function() {
             counter--;
             if(counter < 0) {
-              document.querySelector("#content").innerHTML = "Game over";
+              document.querySelector("#content").innerHTML = ("Game over\n"  );
             }
           }, 1000);
         },
 
+        /*loadPlayerData: function(){
 
+          var p_name = prompt("Sisesta nimi");
+
+          if(p_name ===null || p_name===""){
+            p_name = "Nimeta";
+          }
+
+          this.player.name = p_name;
+          console.log(this.player);
+        },
+*/
         handleMotionRight: function(event){
-          var body = document.body;
+          var body = document.getElementById("content");
           var asd = body.innerText;
+          console.log(asd);
           if (asd== 'Right'){
             var xr_acceleration = event.accelerationIncludingGravity.x;
             if (xr_acceleration < -9 && this.new){
               this.guessed_sides +=1;
+                //this.player.score = this.guessed_sides;
+              Sides.instance.writeScore();
               Sides.instance.writeRandomSide();
               this.new = false;
               window.setTimeout(function(){
                 Sides.instance.new = true;
-              }, 1000);}
-
-          }
-
-
+              }, 500);}
+            }
         },
 
         handleMotionLeft: function(event){
-          var body = document.body;
+          var body = document.getElementById("content");
           var asd = body.innerText;
           if (asd== 'Left'){
             var xl_acceleration = event.accelerationIncludingGravity.x;
             if (xl_acceleration > 9 && this.new){
               this.guessed_sides +=1;
+                //this.player.score = this.guessed_sides;
+              Sides.instance.writeScore();
               Sides.instance.writeRandomSide();
               this.new = false;
               window.setTimeout(function(){
                 Sides.instance.new = true;
-              }, 1000);}
+              }, 500);}
 
           }
-
-
         },
 
         handleMotionFront: function(event){
-          var body = document.body;
+          var body = document.getElementById("content");
           var asd = body.innerText;
           if (asd== 'Front'){
             var zf_acceleration = event.accelerationIncludingGravity.z;
             if (zf_acceleration < -9 && this.new){
               this.guessed_sides +=1;
+                //this.player.score = this.guessed_sides;
+              Sides.instance.writeScore();
               Sides.instance.writeRandomSide();
               this.new = false;
               window.setTimeout(function(){
                 Sides.instance.new = true;
-              }, 1000);}
+              }, 500);}
 
           }
-
-
         },
 
         handleMotionBack: function(event){
-          var body = document.body;
+          var body = document.getElementById("content");
           var asd = body.innerText;
           if (asd== 'Back'){
             var zb_acceleration = event.accelerationIncludingGravity.z;
             if (zb_acceleration > 9 && this.new){
               this.guessed_sides +=1;
+                //this.player.score = this.guessed_sides;
+              Sides.instance.writeScore();
               Sides.instance.writeRandomSide();
               this.new = false;
               window.setTimeout(function(){
                 Sides.instance.new = true;
-              }, 1000);}
+              }, 500);}
 
           }
-
-
         },
 
         handleMotionUp: function(event){
-          var body = document.body;
+          var body = document.getElementById("content");
           var asd = body.innerText;
           if (asd== 'Up'){
             var yu_acceleration = event.accelerationIncludingGravity.y;
             if (yu_acceleration > 9 && this.new){
               this.guessed_sides +=1;
+                //this.player.score = this.guessed_sides;
+              Sides.instance.writeScore();
               Sides.instance.writeRandomSide();
               this.new = false;
               window.setTimeout(function(){
                 Sides.instance.new = true;
-              }, 1000);}
+              }, 500);}
 
           }
-
-
         },
 
         handleMotionDown: function(event){
-          var body = document.body;
+          var body = document.getElementById("content");
           var asd = body.innerText;
           if (asd== 'Down'){
             var yd_acceleration = event.accelerationIncludingGravity.y;
             if (yd_acceleration < -9 && this.new){
               this.guessed_sides +=1;
+                //this.player.score = this.guessed_sides;
+              Sides.instance.writeScore();
               Sides.instance.writeRandomSide();
               this.new = false;
               window.setTimeout(function(){
                 Sides.instance.new = true;
-              }, 1000);}
+              }, 500);}
 
           }
-
-
         },
 
 
@@ -176,6 +194,9 @@
             //leia random indeksiga side
             var random_side = this.sides_list[parseInt(Math.random()*this.sides_list.length)];
             document.querySelector("#content").innerHTML = random_side;
+        },
+        writeScore: function(){
+          document.querySelector("#skoor").innerHTML = "SCORE: " + this.guessed_sides;
         }
     }; // sides LÕPP
 
